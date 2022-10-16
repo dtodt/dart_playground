9ac504adf4cbbea4af58d277288957ebf0ec69da
Daniel Otavio Todt <danieltodt@gmail.com>
2022-10-16 16:06:49 -0300

    refactor: fibonacci reuse and performance

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -1,17 +1,17 @@
 import 'package:dart_playground/lib.dart';
 
-const _kDefaultFibonacciCount = 10;
 const _kInvalidArgumentsMsg = '''
 Invalid challenge, please send the number reference below:
 1 - Fibonacci
 ''';
 
 void main(List<String> arguments) {
-  if (arguments.isNotEmpty && int.tryParse(arguments[0]) != null) {
-    final challenge = int.parse(arguments[0]);
+  final challengeArgument = _getValueFrom(arguments, 0);
+  if (arguments.isNotEmpty && int.tryParse(challengeArgument!) != null) {
+    final challenge = int.parse(challengeArgument);
     switch (challenge) {
       case 1:
-        chooseFibonacciSequence(arguments);
+        _fibonacciSequence(_getRestFrom(arguments, 1));
         break;
       default:
         break;
@@ -22,23 +22,31 @@ void main(List<String> arguments) {
   print(_kInvalidArgumentsMsg);
 }
 
-void chooseFibonacciSequence(List<String> arguments) {
-  int? desiredCount;
-  if (arguments.length > 1) {
-    desiredCount = int.tryParse(arguments[1]) ?? _kDefaultFibonacciCount;
+void _fibonacciSequence(List<String> arguments) {
+  final countArgument = _getValueFrom(arguments, 0);
+  final limitArgument = _getValueFrom(arguments, 1);
+  final algorithmArgument = _getValueFrom(arguments, 2);
+
+  int? count;
+  if (countArgument != null) {
+    count = int.tryParse(countArgument);
+  }
+  int? limit;
+  if (limitArgument != null) {
+    limit = int.tryParse(limitArgument);
   }
 
-  printFibonacciSequence(desiredCount ?? _kDefaultFibonacciCount);
+  printFibonacciSequence(
+    count: count,
+    limit: limit,
+    algorithm: algorithmArgument,
+  );
 }
 
-void printFibonacciSequence(int desiredCount) {
-  final startingPoint = DateTime.now();
-  print('*** Fibonacci ***');
-  if (desiredCount > 40) {
-    print(buildFibonacciFastDoublingSequence(desiredCount));
-  } else {
-    print(buildFibonacciSequence(desiredCount));
-  }
-  final totalTime = DateTime.now().difference(startingPoint).inSeconds;
-  print('*** Total time: $totalTime s ***');
+String? _getValueFrom(List<String> list, int index) {
+  return list.length <= index ? null : list[index];
+}
+
+List<String> _getRestFrom(List<String> list, int startingPoint) {
+  return list.length <= startingPoint ? [] : list.sublist(startingPoint);
 }
